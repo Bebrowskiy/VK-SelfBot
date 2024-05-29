42cb0d9cca02788a115ebd31278fb01654e84fde
Вячеслав Бебровский <127251894+Bebrowskiy@users.noreply.github.com>
2024-05-29 23:16:10 +1200

    Update has_prefix_middleware.py

--- selfBot/middlewares/has_prefix_middleware.py
+++ selfBot/middlewares/has_prefix_middleware.py
@@ -1,5 +1,6 @@
 import json
 from vkbottle import BaseMiddleware
+from time import sleep
 
 
 class HasPrefixMiddleware(BaseMiddleware):
@@ -7,8 +8,12 @@ class HasPrefixMiddleware(BaseMiddleware):
     Миддлварь, который отсеивает сообщения,
     которые не начинаются с префикса
     """
+
     async def pre(self):
         with open("config.json", "r", encoding="utf-8") as file:
             content = json.load(file)
-        if not self.event.text.startswith(content["prefix"]):
-            self.stop("Сообщение не начинается с префикса")
+        if content.get("ai_mode", True):
+            return
+        else:
+            if not self.event.text.startswith(content["prefix"]):
+                self.stop("Сообщение не начинается с префикса")
